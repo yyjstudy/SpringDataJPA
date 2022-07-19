c58ddbf9b28f3e79e0a9a62872eb3d25c521eb9a
yongjo <yjlee90@suresofttech.com>
2022-07-19 11:59:15 +0900

    쿼리 메소드 기능-JPA Hint & Lock

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -255,4 +255,32 @@ class MemberRepositoryTest {
         }
 
     }
+
+    @Test
+    public void queryHInt() {
+        //given
+        Member member1 = new Member("member1", 10);
+        memberRepository.save(member1);
+        em.flush();
+        em.clear();
+
+        //when
+        Member findMember = memberRepository.findReadOnlyByUsername(member1.getUsername());
+        findMember.setUsername("member2");
+
+        em.flush();
+    }
+
+    @Test
+    public void lock() {
+        //given
+        Member member1 = new Member("member1", 10);
+        memberRepository.save(member1);
+        em.flush();
+        em.clear();
+
+        //when
+        List<Member> result = memberRepository.findLockByUsername("member1");
+    }
+
 }
