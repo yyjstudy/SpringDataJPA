26860f4d2daffe3a018e574f92be991978cb700e
yongjo <yjlee90@suresofttech.com>
2022-07-19 12:38:40 +0900

    확장 기능-사용자 정의 리포지토리 구현

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -283,4 +283,9 @@ class MemberRepositoryTest {
         List<Member> result = memberRepository.findLockByUsername("member1");
     }
 
+    @Test
+    public void callCustom() {
+        List<Member> result = memberRepository.findMemberCustom();
+
+    }
 }
